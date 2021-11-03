8df2769cbb1f9e9138de801e76ce1905831bc51c
toptops <seongtab@naver.com>
2021-11-04 03:56:49 +0900

    bug fixed

--- src/main/java/com/top/toypreference/config/db/annotation/EnableToyJPA.java
+++ src/main/java/com/top/toypreference/config/db/annotation/EnableToyJPA.java
@@ -8,6 +8,6 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import({ToyDatasourceConfig.class, ToyJPAConfig.class})
+@Import({ToyJPAConfig.class})
 public @interface EnableToyJPA {
 }
