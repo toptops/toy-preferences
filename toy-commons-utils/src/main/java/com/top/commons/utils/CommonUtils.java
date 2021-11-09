0e9b45fac2b655342f7861985dd18fb2d4bfca74
toptops <seongtab@naver.com>
2021-11-09 16:52:57 +0900

    response 약한 참조 객체 만드는 유틸함수 추가

--- toy-commons-utils/src/main/java/com/top/commons/utils/CommonUtils.java
+++ toy-commons-utils/src/main/java/com/top/commons/utils/CommonUtils.java
@@ -1,8 +1,14 @@
 package com.top.commons.utils;
 
+import java.lang.ref.WeakReference;
+
 public class CommonUtils {
 
     public static String hello() {
         return "hello";
+    }
+
+    public static <T> T getWeakReference(T t) {
+        return new WeakReference<>(t).get();
     }
 }
