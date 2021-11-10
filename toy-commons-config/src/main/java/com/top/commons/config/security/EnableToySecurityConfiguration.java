96cf65bf7dad6cf5fe38f8f53aba7e5df7629617
toptops <seongtab@naver.com>
2021-11-10 20:42:03 +0900

    JwtAuthFilter import 추가

--- toy-commons-config/src/main/java/com/top/commons/config/security/EnableToySecurityConfiguration.java
+++ toy-commons-config/src/main/java/com/top/commons/config/security/EnableToySecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.top.commons.config.security;
 
 import com.top.commons.config.security.basic.ToyUserDetailServiceRegister;
 import com.top.commons.config.security.basic.ToyUserDetailsService;
+import com.top.commons.config.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
@@ -9,7 +10,8 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import({ToyUserDetailServiceRegister.class})
+@Import({ToyUserDetailServiceRegister.class,
+        JwtAuthenticationFilter.class})
 public @interface EnableToySecurityConfiguration {
     Class<? extends ToyUserDetailsService> detailsService();
 }
